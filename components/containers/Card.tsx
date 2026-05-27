45f51669180266fe5d314ac78132c7097400a896
Christian Sturm <19sturm_der_liebe96@proton.me>
2026-05-27 20:26:27 +0200

    fix: remove aspect ratio from card

--- components/containers/Card.tsx
+++ components/containers/Card.tsx
@@ -6,7 +6,7 @@ type CardProps = PropsWithChildren<{ title: string, text: ReactNode }>
 export default function Card({title, text, children}: CardProps) {
     return (
         <section
-            className="w-343/375 xl:w-420/1440 aspect-420/430 bg-background-default flex flex-col items-stretch p-10 shadow-z24 rounded-2xl">
+            className="w-343/375 xl:w-420/1440 bg-background-default flex flex-col items-stretch p-10 shadow-z24 rounded-2xl">
             <div className="pb-6">
                 <H4>{title}</H4>
                 {text}
